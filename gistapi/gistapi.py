2fe9c74e5d017c27332c228b4c0ad8843c611641
malinova <malinova@gmail.com>
2021-02-24 23:08:58 +0200

    All bonuses implemented.

--- gistapi/gistapi.py
+++ gistapi/gistapi.py
@@ -12,11 +12,27 @@ providing a search across all public Gists for a given Github account.
 import requests
 import re
 from flask import Flask, jsonify, request
-
+from redis import Redis
+import traceback
+import os
+import json
 
 # *The* app object
 app = Flask(__name__)
 
+if os.environ.get("WITH_REDIS"):
+    redis_client = Redis(host='cache', port=6379, decode_responses=True)
+else:
+    redis_client = None
+
+class ExternalError(Exception):
+    pass
+
+class UserNotFound(Exception):
+    pass
+
+class BadOrMissingParameter(Exception):
+    pass
 
 @app.route("/ping")
 def ping():
@@ -38,16 +54,88 @@ def gists_for_user(username):
         The dict parsed from the json response from the Github API.  See
         the above URL for details of the expected structure.
     """
-    gists_url = 'https://api.github.com/users/{username}/gists'.format(
-            username=username)
-    response = requests.get(gists_url)
+
+    per_page=100
+    page=1
+    gists_url = 'https://api.github.com/users/{username}/gists?per_page={per_page}&page={page}'.format(
+            username=username, per_page=per_page, page=page)
+    
+
+    all_gists = []
+
     # BONUS: What failures could happen?
-    # BONUS: Paging? How does this work for users with tons of gists?
+    try:
+        response = requests.get(gists_url)
+        response.raise_for_status()
+        all_gists = response.json()
 
-    return response.json()
+        # BONUS: Paging? How does this work for users with tons of gists?
+        # The GitHub API does not return pagination information
+        # so retrieve while there is still content
+        # total limit is 3000 (GitHub API docs)
+        while len(response.json()) == per_page and page <= 30:
+            page += 1
+            gists_url = 'https://api.github.com/users/{username}/gists?per_page={per_page}&page={page}'.format(
+            username=username, per_page=per_page, page=page)
+            response = requests.get(gists_url)
+            response.raise_for_status()
+            all_gists += response.json()
+        
+    except:
+        raise ExternalError()
 
-def fetch_gist_content(raw_url):
-    return requests.get(raw_url)
+    return all_gists
+
+def fetch_file_content(raw_url):
+    """ Fetches the text content of a single file from a Gist.
+
+        If running in Docker and redis is available, check and use cache.
+    """
+    if redis_client:
+        check_cache = redis_client.get(raw_url)
+    else:
+        check_cache = None
+    
+    if check_cache != None:
+        return check_cache
+    else:
+        r = requests.get(raw_url)
+
+        if redis_client:
+            redis_client.set(raw_url, r.text)
+        
+        return r.text
+
+
+def fetch_single_gist(url):
+    """ Fetches the JSON content of a single Gist Object.
+
+        If running in Docker and redis is available, check and use cache.
+    """
+
+    if redis_client:
+        check_cache = redis_client.get(url)
+    else:
+        check_cache = None
+    
+    if check_cache != None:
+        return json.loads(check_cache)
+    else:
+        r = requests.get(url)
+
+        if redis_client:
+            redis_client.set(url, json.dumps(r.json()))
+
+        return r.json()
+
+def check_user_exists(username):
+    """ Check if a user exists.
+        Raise error if user does not exist.
+    """
+    url = 'https://api.github.com/users/{username}'.format(username=username)
+    r = requests.get(url)
+    if r.status_code == 404:
+        raise UserNotFound()
 
 @app.route("/api/v1/search", methods=['POST'])
 def search():
@@ -61,37 +149,82 @@ def search():
         object contains the list of matches along with a 'status' key
         indicating any failure conditions.
     """
-    post_data = request.get_json()
-    # BONUS: Validate the arguments?
+    try:
+        post_data = request.get_json()
 
-    username = post_data['username']
-    pattern = post_data['pattern']
+        # BONUS: Validate the arguments?
+        if post_data['username']==None or post_data['pattern']==None:
+            raise BadOrMissingParameter()
 
-    result = {}
-    gists = gists_for_user(username)
-    # BONUS: Handle invalid users?
+        username = post_data['username']
+        pattern = post_data['pattern']
 
-    matching=[]
-    for gist in gists:
-        # REQUIRED: Fetch each gist and check for the pattern
-        for filename in gist[u"files"].keys():
-            content = fetch_gist_content(gist[u'files'][filename][u'raw_url'])
-            match = re.match(pattern, content.text)
-            if match:
-                matching.append(gist[u'html_url'])
-                break
+        try:
+            re.compile(pattern)
+        except:
+            raise BadOrMissingParameter()
 
-        # BONUS: What about huge gists?
-        # BONUS: Can we cache results in a datastore/db?
-        
+        result = {}
 
-    result['status'] = 'success'
-    result['username'] = username
-    result['pattern'] = pattern
-    result['matches'] = matching
+        # BONUS: Handle invalid users?
+        check_user_exists(username)
 
-    return jsonify(result)
+        gists = gists_for_user(username)
+
+        matching=[]
+        for gist in gists:
+            # REQUIRED: Fetch each gist and check for the pattern
+            gist_full = fetch_single_gist(gist[u"url"])
+
+            for filename in gist_full[u"files"].keys():
+                file_object = gist_full[u'files'][filename]
+                file_text=None
+
+                # BONUS: What about huge gists?
+                if file_object[u'truncated']:
+                    file_text=fetch_file_content(file_object[u'raw_url'])
+                else:
+                    file_text=file_object[u"content"]
+
+                match = re.match(pattern, file_text)
+                if match:
+                    matching.append('https://gist.github.com/{username}/{id}'.format(username=username, id=gist_full[u"id"]))
+                    break
+
+
+            # BONUS: Can we cache results in a datastore/db?
+            # ===> see fetch_file_content() and fetch_single_gist()
+
+        result['status'] = 'success'
+        result['username'] = username
+        result['pattern'] = pattern
+        result['matches'] = matching
+        return jsonify(result)
+    
+    # Simulate exception layer
+    except UserNotFound:
+        result['status'] = 'error'
+        result['code'] = 400
+        result['message'] = "Invalid username"
+        return result, 400
+    except BadOrMissingParameter:
+        result['status'] = 'error'
+        result['code'] = 400
+        result['message'] = "Bad or missing parameter"
+        return result, 400
+    except ExternalError:
+        result['status'] = 'error'
+        result['code'] = 500
+        result['message'] = "Information could not be retrieved from Github"
+        return result, 500
+    except:
+        # Print stacktrace of other errors for debugging
+        traceback.print_exc()
+        result['status'] = 'error'
+        result['code'] = 500
+        result['message'] = "Your request could not be processed"
+        return result, 500
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=True, host='0.0.0.0', port=8000, threaded=True)
